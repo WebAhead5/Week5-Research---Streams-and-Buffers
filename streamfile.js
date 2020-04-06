77a6e18c3a8774e13ffbccc09cf245694b31cb5b
James Daniels <jrd656@gmail.com>
2020-04-06 14:46:32 +0300

    it's working

--- streamfile.js
+++ streamfile.js
@@ -1,6 +1,14 @@
 //To start with, you could hardcode the file path bigtextfile.txt into the js file instead of passing it as a command-line argument.
 
-let fs = require('fs');
-var text = fs.readFileSync("./bigtextfile.txt");
-var textByLine = text.split("\n");
+var fs = require('fs');
+var http = require('http');
+
+var myReadStream = fs.createReadStream(__dirname + '/bigtextfile.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk received:');
+    console.log(chunk)
+})
+
+
 
